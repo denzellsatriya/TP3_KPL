70e926d3dcc4de49fc05bc724e389601136fe7ef
Denzell Satriya Putera Wibisono <denzellsatriya@gmail.com>
2026-03-08 16:59:47 +0700

    first commit

--- TP_MODUL3_103022400034/Form1.cs
+++ TP_MODUL3_103022400034/Form1.cs
@@ -6,5 +6,10 @@ namespace TP_MODUL3_103022400034
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
